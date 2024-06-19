8f97979e75c27e636f21de3fd17b3cb4bc4ed74e
Anand Panda <anand612p@gmail.com>
2024-06-20 05:11:17 +0530

    connected to database using prisma orm and neon

---
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -0,0 +1,18 @@
+interface DesignConfiguratorProps {
+	configId: string;
+	imageUrl: string;
+	imageDimensions: {
+		width: number;
+		height: number;
+	};
+}
+
+const DesignConfigurator = ({
+	configId,
+	imageUrl,
+	imageDimensions,
+}: DesignConfiguratorProps) => {
+	return <div></div>;
+};
+
+export default DesignConfigurator;
